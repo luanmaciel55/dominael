a407b66be952069ce514aed902619aef0bdbb0ee
luanmaciel55 <contato.luanmaciel@gmail.com>
2026-09-11 17:26:59 -0300

    Cria loja Dominael com InfinitePay, Supabase e entregas digitais

---
+++ assets/config.js
@@ -0,0 +1,5 @@
+window.DOMINAEL_CONFIG = {
+  supabaseUrl: "https://lzhgwhrlpydwbcglcjrj.supabase.co",
+  supabasePublishableKey: "sb_publishable_ejIeqJWG0QIktJ_kG9hfxw_womJRMwa",
+  functionsBase: "https://lzhgwhrlpydwbcglcjrj.supabase.co/functions/v1"
+};
